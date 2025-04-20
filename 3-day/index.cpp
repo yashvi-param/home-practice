2b268fa6e35be9dcfc4b81031195ffd7d7c385e6
yashvi-param <yashvirwn123@gmail.com>
2025-04-20 09:54:19 +0530

    Add files via upload
    day-3 practice complete

---
+++ 3-day/index.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    //int a;
+    //int b;
+    //cin >> a;
+    //cin >> b;
+
+
+    //cout << "addition of a & b" << a+b << endl;
+    //cout << "substeaction of a & b" << a-b << endl;
+    //cout << "devidation of a & b" << a/b << endl;
+    //cout << "multiplication of a & b" << a*b << endl;
+    //cout << "moduls of a & b" << a % b << endl;
+
+    //int x=10;
+    //int y=20;
+    //int z=5;
+
+    //relational operators
+
+
+// x < y true;
+// x > y false;
+// x == y false;
+// x <= y true;
+// x >= y false;
+// x != y true;
+
+
+// logical relational
+
+
+// 10 == 20 && 20 == 20; false
+// 10 == 20 || 20 == 20; TRUE
+// 10 == 20 || 20 == 30; TRUE
+
+// X < Y && Y < Z;
+// X < Y && Y < Z;
+
+
+// ASSIGNMENTAL OPERATORS
+
+// Z = X + Y; 30
+
+//INT A=100;
+//INT B=50;
+
+
+// b=a+b;
+// b+=a;
+
+ //int num = 100000000000000;
+
+
+ //cout << sizeof(b) << endl;
+ //cout << sizeof(num) << endl;
+
+ int n=20;
+ int n2=20;
+ n++; //21
+ n++;  //22
+ n++;  //23
+
+
+ --n2;//19
+ --n2;//18
+ n2--;//17
+
+ //int num3 = 5;
+ //int num4 = 5;
+
+
+// cout << n << " " << n2 <<endl;
+
+cout << 1 + 8 * 8 - 5 / 6 + 2 * 2 / 4 << endl;
+
+}
