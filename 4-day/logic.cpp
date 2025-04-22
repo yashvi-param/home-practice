b30924e351810151c8369ba79c10f920fba2bb25
yashvi-param <yashvirwn123@gmail.com>
2025-04-22 12:50:22 +0530

    Add files via upload
    complete

---
+++ 4-day/logic.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int a = 100;
+    int b = 20;
+
+
+    if(a>b)
+    {
+        cout << "a is bigger.....!" << endl;
+    }
+    else if (b>a)
+    {
+        cout << "b is bigger...!" << endl;
+    }
+    else
+    {
+        cout << "a and b are equal...!" << endl;
+    }
+
+
+
+    return 0;
+}
