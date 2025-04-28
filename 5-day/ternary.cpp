059b118c13783588ff17e4b673a5a210b563e376
yashvi-param <yashvirwn123@gmail.com>
2025-04-28 21:08:59 +0530

    Add files via upload
    complete

---
+++ 5-day/ternary.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+
+ int main(){
+
+    // int value = 0;
+
+    // if(value)
+    // {
+    //     cout << "RIGHT" << endl;
+    // }
+    // else 
+    // {
+    //     cout << "WRONG" << endl;
+    // }
+
+
+    // value ?
+    //  cout << "RIGHT" <<endl 
+    //  :
+    //   cout << "WRONG" << endl;
+
+
+
+
+
+
+
+// int choice = 1;
+
+// (choice==1) ?
+//     cout << "Pizza" << endl
+//     : (choice==2) ?
+//     cout << "Sandwitch" << endl
+//     : (choice ==3) ?
+//     cout << "burger" <<endl
+//     :
+//     cout << "valid choic" << endl;
+
+
+
+
+
+
+
+
+
+
+
+int x = 5;
+int y = 10;
+int z = 20;
+
+
+
+
+
+    (x>y) ?
+        (x>z) ? cout << "x is big" <<endl : cout << "z is big" << endl
+        : 
+        (y>z) ? cout << "Y is big" <<endl : cout << "z is big" << endl;
+
+
+    return 0;
+ }
