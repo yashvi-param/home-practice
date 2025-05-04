f444b9f8fa11173ba2858dc2d93a098998f39dc6
yashvi-param <yashvirwn123@gmail.com>
2025-05-04 09:26:24 +0530

    Add files via upload
    incomplete day-7

---
+++ 7-day/index.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+
+    int x = 1;
+
+     
+    while(x<5)
+    {
+        cout << "table of 2" << x << endl;
+
+        x = x*2;
+    }
+
+
+
+    int i = 1;
+    int sum = 0;
+
+
+    
+}
---
+++ 7-day/index1.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+
+//    int x = 1;
+  
+//    while(x<5) // true 1 
+//    {
+//     cout << "table of 2" << x <<  endl;
+
+//     x = x*2;
+//    }
+
+
+
+
+// int i =1;
+// int sum = 0;
+
+
+// while(i<=5)
+// {
+
+//     sum = sum+i;
+
+//     cout << i << " = " <<  endl;
+
+//     i++;
+// }
+
+// cout << sum << endl;
+
+//     return 0;
+// }
+
+
+
+// 2 * 1 = 2
+// 2 *2 = 4
+// 2 *3 =6
+// 2 *4 = 8
+
+
+
+// int i = 1;
+
+// int num ;
+// cout << "Enter num: ";
+// cin >> num;
+
+// while(i<=10)
+// {
+//     cout << num << " x " << i << " = " << num*i << endl;
+
+//     i++;
+
+// }
+
+
+}
